859a75c77055d7debe51d71269b6bdb1b48d47c4
Kennan Smith <kennan.smith1@gmail.com>
2018-10-15 17:26:01 -0600

    added hover animation to weblinks

--- src/components/Weblink.js
+++ src/components/Weblink.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react"
 import ReactDOM from "react-dom"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 
 let Anchor = styled.a`
     margin: 1vw;
 `
 let Icon = styled.img`
     height: 10vh;
+    opacity: 0.75;
+    transition: 0.2s;
+    &:hover {
+        height: 11vh;
+        opacity: 0.5;
+        transition: 0.2s;
+    }
 `
 
 class WebLink extends Component {
