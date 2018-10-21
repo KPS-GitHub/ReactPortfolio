89c28eb0534b1d2584395a364a7eecd1e1ebd83b
Kennan Smith <kennan.smith1@gmail.com>
2018-10-21 12:04:31 -0600

    vertically centered 'carousel.caption' component in carousel.js - will likely need to be changed when placeholder text is replaced with content

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -33,21 +33,21 @@ class ControlledCarousel extends React.Component {
         >
           <Carousel.Item>
             <img style={{height: '80vh'}} alt="900x500" src={blackBg} />
-            <Carousel.Caption>
+            <Carousel.Caption style={{bottom: '30vh'}}>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img style={{height: '80vh'}} alt="900x500" src={blackBg} />
-            <Carousel.Caption>
+            <Carousel.Caption style={{bottom: '30vh'}}>
               <h3>Second slide label</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img style={{height: '80vh'}} alt="900x500" src={blackBg} />
-            <Carousel.Caption>
+            <Carousel.Caption style={{bottom: '30vh'}}>
               <h3>Third slide label</h3>
               <p>
                 Praesent commodo cursus magna, vel scelerisque nisl consectetur.
