63a267b2477838d7fcafeb8cc8ee6b764b9124d6
Kennan Smith <kennan.smith1@gmail.com>
2018-10-31 18:32:21 -0600

    added overlay to pfpiece component - is simply a black background so next step is to give it images of the logos for the technologies used as well as a message on the slide that alerts the user of this

--- src/components/carouselSlides/portfolio/PfPiece.js
+++ src/components/carouselSlides/portfolio/PfPiece.js
@@ -2,11 +2,22 @@ import React, { Component } from "react"
 import styled from "styled-components"
 
 let Anchor = styled.a`
-    margin: 1.5vh 1vw .5vh 1vw;
+    
 `
 
 let Image = styled.img`
     height: 10vh;
+`
+
+let Overlay = styled.div`
+    margin: 1.5vh 1vw .5vh 1vw;
+    background: black;
+    opacity: 1;
+    transition: 0.1s;
+    &:hover {
+        opacity: 0.5;
+        transition: 0.2s;
+    }
 `
 
 
@@ -14,10 +25,12 @@ let Image = styled.img`
 class PfPiece extends Component {
     render () {
         return (
-            <Anchor href={this.props.anchorLink}>
-                <Image src={this.props.imageLink}>
-                </Image>
-            </Anchor>
+            <Overlay>
+                <Anchor href={this.props.anchorLink}>
+                    <Image src={this.props.imageLink}>
+                    </Image>
+                </Anchor>
+            </Overlay>
         )
     }
 }
